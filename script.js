9746b9daa99988db4431343052d014793dfa9ec5
Sergio Piniella <piniella,sergio@yandex.ru>
2024-12-06 11:00:56 +0300

    Начальное решение задачи

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
-function calculateDelivery() {}
+function calculateDelivery(distance, typeDelivery, packing) {
+  const tarif = 5;
+  let ratioDelivery = null;
+  let ratioPacking = null;
 
-calculateDelivery(5);
-calculateDelivery(7, "standard", true);
-calculateDelivery(10, "express", false);
+  if (typeDelivery === "standard") {
+    ratioDelivery = 1;
+  } else {
+    ratioDelivery = 1.3;
+  }
+
+  if (packing === false) {
+    ratioPacking = 0;
+  } else {
+    ratioPacking = 15;
+  }
+
+  return distance * tarif * ratioDelivery + ratioPacking;
+}
+
+console.log(calculateDelivery(10, "standard", false));
+console.log(calculateDelivery(10, "standard", true));
+console.log(calculateDelivery(10, "express", false));
+console.log(calculateDelivery(10, "express", true));
